1a4d52712e02c029830ca3f155c1315b50064295
AbhraaoMota <149906527+AbhraaoMota@users.noreply.github.com>
2024-01-30 22:23:22 -0300

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,18 @@
+ const botao = document.querySelector(".btn-plataforma");
+
+ const elementoPlataforma = document.querySelector(".btn-plataforma .plataforma");
+
+ botao.addEventListener("click", () => {
+
+    elementoPlataforma.classList.toggle("ativo");
+
+    // const botaoEstaAberto = elementoPlataforma.classList.contains("ativo")
+    
+    // if(botaoEstaAberto) {
+    //     elementoPlataforma.classList.remove("ativo")
+    // }else{
+    //     elementoPlataforma.classList.add("ativo")
+    // }
+
+ });
+ 
